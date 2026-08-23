feae3716cb38089df38428a28247bf7918831e67
xjamal21 <jayz445hood@gmail.com>
2026-08-23 16:10:58 +0800

    game logic
    testing enemy hurt effects and added basic weapon mechanic

--- gdscripts/enemies.gd
+++ gdscripts/enemies.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+var health = 3
+
 var player
 
 func _ready() -> void:
@@ -9,3 +11,12 @@ func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 100.0
 	move_and_slide()
+
+func take_damage():
+	
+	health -= 1
+	
+	if health == 0:
+		queue_free()
+	else:
+		$AnimatedSprite2D.play("hurt")
