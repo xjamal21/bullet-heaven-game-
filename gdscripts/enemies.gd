7546a050b4ec100f90f08e3e299821b67b99211d
xjamal21 <jayz445hood@gmail.com>
2026-08-23 16:26:10 +0800

    border
    yea border (not working)

--- gdscripts/enemies.gd
+++ gdscripts/enemies.gd
@@ -6,11 +6,13 @@ var player
 
 func _ready() -> void:
 	player = get_node("/root/Game/Player")
+	
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 100.0
 	move_and_slide()
+	
 
 func take_damage():
 	
--- gdscripts/player.gd
+++ gdscripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var map_min: Vector2 = Vector2(0, 0)
+@export var map_max: Vector2 = Vector2(1152, 648)
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -10,3 +12,8 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = direction * 300
 	move_and_slide()
+	
+	var viewport_rect = get_viewport_rect()
+	var margin = 16.0
+	global_position.x = clamp(global_position.x, viewport_rect.position.x + margin, viewport_rect.end.x - margin)
+	global_position.y = clamp(global_position.y, viewport_rect.position.y + margin, viewport_rect.end.y - margin)
