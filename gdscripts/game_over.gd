7e2d95bb85ec0348e4d853baac7b1585613537b5
xjamal21 <jayz445hood@gmail.com>
2026-08-25 18:34:56 +0800

    LMFAOFAOFOAFOAFOAOFOAFOA
    LMAOFOFOAOFAOFOAFOAFOAOFAOFOA

--- gdscripts/game_over.gd
+++ gdscripts/game_over.gd
@@ -8,4 +8,5 @@ func game_over():
 	self.show()
 	
 func _on_restart_pressed() -> void:
-	get_tree().reload_current_scene()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://tscnfiles/main_game.tscn")
